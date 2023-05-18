ef5a3050344d76b335c327950d3e8c008a03d9f3
MAXAKAMVP <maxvillalonga@hotmail.com>
2023-05-18 18:24:28 +0200

    Second commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
-def hello():
+def index():
+    return render_template("index.html")
+
+@app.route("/purchase")
+def purchase():
     pass
+    #return render_template("purchase.html")
+
+@app.route("/status")
+def status():
+    pass
+    #return render_template("status.html")
